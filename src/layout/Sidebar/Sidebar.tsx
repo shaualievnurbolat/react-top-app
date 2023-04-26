c965dbc3e0f21f5d887c399dd2c895dec6b70b0c
nurbolat <shaualiev.ns@gmail.com>
2023-04-27 01:32:36 +0600

    file: add files

---
+++ src/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+ 
+import { SidebarProps } from "./Sidebar.props"
+import styles from './Sidebar.module.css'
+import cn from "classnames"
+import { Menu } from "../Menu/Menu"
+
+export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+    return (
+        <div {...props}>
+           <Menu />
+        </div>
+    )
+}
